9ad65c4a5dbec772526239ce0f5c1c85e851eb5a
OpponentArtsiom <opponentartem@gmail.com>
2023-06-07 15:20:28 +0300

    ервое изменение

--- test.py
+++ test.py
@@ -2,3 +2,9 @@ for hour in range(24):
     for minutes in range(60):
         for seconds in range(60):
             print(hour, ':', minutes, ':', seconds)
+
+num = int(input())
+if num % 2 == 0:
+    print("Число четное")
+else:
+    print("Число не четное")
